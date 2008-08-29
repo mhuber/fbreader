73087f0d94d40574884dc2c8a7c264d39fa7232c
Alexander Kerner <lunohod@gmx.de>
2008-08-29 18:11:01 +0200

    disable config autosave function

--- zlibrary/core/src/unix/xmlconfig/XMLConfig.cpp
+++ zlibrary/core/src/unix/xmlconfig/XMLConfig.cpp
@@ -197,5 +197,5 @@ bool XMLConfig::isAutoSavingSupported() const {
 }
 
 void XMLConfig::startAutoSave(int seconds) {
-	ZLTimeManager::instance().addTask(mySaver, 1000 * seconds);
+//	ZLTimeManager::instance().addTask(mySaver, 1000 * seconds);
 }
